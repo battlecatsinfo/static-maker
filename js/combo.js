82b5e25e70d14ff0373753ef68f5cffbf0185d73
battlecatsinfo <ianfun.github.io@gmail.com>
2024-08-06 21:19:42 +0800

    remove files

--- js/combo.js
+++
@@ -1,8 +0,0 @@
-new (class extends require('./base.js') {
-	constructor() {
-		super();
-		this.write_template('html/combos.html', 'combos.html', {
-			'combos': this.load('combos.html')
-		});
-	}
-})();
